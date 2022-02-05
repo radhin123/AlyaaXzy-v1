ad5dd8d475408f07a018dbb0b4f7cb35a654885a
Radhin Al haady <78122715+radhin123@users.noreply.github.com>
2022-02-05 21:17:36 +0700

    update main.js

--- main.js
+++ main.js
@@ -24,7 +24,7 @@ isCharge: "Sedang di cas" || "Tidak di cas"
 async function starts() {
 	    client.autoReconnect = ReconnectMode.onConnectionLost
 	    client.version = [2, 2143, 3]
-	    client.browserDescription = ["AlyaaXzy","firefox","3.0"]
+	    client.browserDescription = ["AlyaaXzy X Botcah™","firefox","3.0"]
 	    client.logger.level = 'warn'
 	    console.log(color(`]─`,`magenta`),`「`,  color(`AlyaaXzy`,`red`), `」`,  color(`─[`,`magenta`))
 	    client.on('qr', () => {
